4786e0c1d0ea7b31ae93b0f8823841ff65ed3742
Shubham Sharma <shubham.asr101@gmail.com>
2026-05-07 22:03:32 +0530

    DataBase level validations

--- src/app.js
+++ src/app.js
@@ -57,19 +57,20 @@ app.delete("/user", async (req, res) => {
         res.status(400).send("Something went wrong...")
     }
 })
-// update 
+// update the user data
 app.patch("/user", async (req, res) => {
     const userId = req.body.userId;
     const data = req.body;
     try {
         const user =   await User.findByIdAndUpdate({ _id: userId }, data,{
-            returnDocument : 'after'
+            returnDocument : 'after',
+            runValidators : true
          })
-        res.send("User Updated Successfully::::",user)
+        res.send("User Updated Successfully::::")
         console.log(user);
         
     } catch (err) {
-        res.status(400).send("Something went wrong...")
+        res.status(400).send("Update Failed:" + err.message)
     }
 })
 connectDB().then(() => {
