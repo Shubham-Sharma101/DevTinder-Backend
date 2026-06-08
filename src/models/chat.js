c0e9c7bae3298daff3ed228761ce708bc36fbb93
Shubham Sharma <shubham.asr101@gmail.com>
2026-06-08 18:35:24 +0530

    Added user details in chat

--- src/models/chat.js
+++ src/models/chat.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const chatSchema = new mongoose.Schema({
       message :{
         type: String,
+      },
+      fromUserId :{
+        
       }
 })
 const Chat = mongoose.model("chat", chatSchema);
